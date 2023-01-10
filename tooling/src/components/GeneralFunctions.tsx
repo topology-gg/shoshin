b5971d3f8c3fb703a45d8a5930bf368db41483f7
Gregory Edison <gregory.edison1993@gmail.com>
2023-01-10 16:50:25 +0100

    feat: styling

--- tooling/src/components/GeneralFunctions.tsx
+++ tooling/src/components/GeneralFunctions.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import styles from '../../styles/Home.module.css'
 import { Box, Grid } from "@mui/material";
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import TextField from '@mui/material/TextField';
+import { FunctionElement, Operator } from '../types/Function'
 
 const cardStyle = {
     display: 'flex', 
@@ -25,17 +28,27 @@ const gridItemStyle = {
     m: '1px',
 }
 
-const functions_title = [{id: 'Operators', width: 5}, {id: 'Const', width: 1}, {id: 'Perceptibles', width: 5}]
-const operators = ['AND', 'OR', '/', '*', '%', '==', '()']
+const functions_title = [
+    { id: 'Operators', width: 5 }, 
+    { id: 'Const', width: 1 }, 
+    { id: 'Perceptibles', width: 5 }
+]
+const operators = Object.values(Operator)
 
 const handleDrag = (e) => {
     document.body.style.cursor = 'grabbing';
+    let source = e.target.id.split('-')
+    switch (source[0]) {
+        case 'operator': {
+            currentDraggedItem = { value: source[1] } as FunctionElement
+        }
+    }
     e.preventDefault()
     e.stopPropagation()
 }
 const handleDragEnd = (e) => {
-    console.log('drag end')
     document.body.style.cursor = 'default';
+    currentDraggedItem = {}
     e.preventDefault()
     e.stopPropagation()
 }
@@ -43,14 +56,14 @@ const handleDragOver = (e) => {
     e.preventDefault()
     e.stopPropagation()
 }
-const handleDrop = (e) => {
-    document.body.style.cursor = 'default';
-    e.preventDefault()
-    e.stopPropagation()
-}
 
-const GeneralFunctions = ({}) => {
-    let f = "Drop your operators here"
+let currentDraggedItem = {} as FunctionElement
+let currentConstant = 0
+
+const GeneralFunctions = ({ 
+    functions, handleUpdateGeneralFunction, handleConfirmFunction, functionsIndex, setFunctionsIndex
+}) => {
+    let f = "Drop your operators, constants and perceptibles here"
     return (
         <Box
         sx={{
@@ -60,44 +73,73 @@ const GeneralFunctions = ({}) => {
             alignItems: "left",
             mt: "2rem",
         }}>
-            <Grid container spacing={0} sx={{display: 'flex', justifyContent: "space-between"}}>
-                <Grid sx={{m: '3px'}} item className='functions-title' xs={12}>
-                    <Grid container sx={{display: 'flex', justifyContent: "space-between"}}>
+            <Grid container spacing={0} sx={{ display: 'flex', justifyContent: "space-between" }}>
+                <Grid sx={{ m: '3px' }} item className='functions-title' xs={ 12 }>
+                    <Grid container sx={{ display: 'flex', justifyContent: "space-between" }}>
                         {
                             functions_title.map((f) => {
-                                return <Grid key={`function-${f.id}`} sx={{p: '5px'}} item xs={f.width}><Typography variant='overline'>{f.id}</Typography></Grid>
+                                return <Grid 
+                                    key={ `function-${f.id}` } 
+                                    sx={{ p: '5px' }} 
+                                    item 
+                                    xs={ f.width }>
+                                        <Typography variant='overline'>{ f.id }</Typography> 
+                                    </Grid>
                             })
                         }
                     </Grid>
                 </Grid>
-                <Grid sx={gridItemStyle} xs={5} item>
+                <Grid sx={ gridItemStyle } xs={ 5 } item>
                     {
                         operators.map((o) => {
                             return <Card 
-                                        key={`operator-${o}`} 
-                                        onDragEnd={(e) => handleDragEnd(e)} 
-                                        onDrag={(e) => handleDrag(e)} 
+                                        key={ `operator-${o}` } 
+                                        id={  `operator-${o}` }
+                                        onDragEnd={ (e) => handleDragEnd(e) } 
+                                        onDrag={ (e) => handleDrag(e) } 
                                         draggable 
-                                        sx={cardStyle}>
+                                        sx={ cardStyle }>
                                             <CardContent>{o}</CardContent>
                                     </Card>
                         })
                     }
                 </Grid>
-                <Grid sx={gridItemStyle} xs={1} item>
-                    Two
+                <Grid style={{ maxWidth: 'none', flexGrow: 1 }} sx={ { ...gridItemStyle }} xs={ 1 } item>
+                    <TextField 
+                        color={ "info" } 
+                        id="constant" 
+                        type="number" 
+                        InputLabelProps={{ shrink: true }}
+                        defaultValue={currentConstant}
+                        draggable
+                        onChange={(e) => currentConstant=parseInt(e.target.value)}
+                    />
                 </Grid>
-                <Grid sx={gridItemStyle} xs={5} item>
+                <Grid sx={ gridItemStyle } xs={ 5 } item>
                     Three
                 </Grid>
-                <Grid sx={{...gridItemStyle, mt: '2rem'}} xs={12} item className='function-creator'>
+                <Grid sx={{ ...gridItemStyle, mt: '2rem' }} xs={ 12 } item className='function-creator'>
                     <Card 
-                        style={{border: 'none', boxShadow: 'none'}} 
-                        onDragOver={(e) => handleDragOver(e)}
-                        onDrop={(e) => handleDrop(e)}
+                        style={{ border: 'none', boxShadow: 'none' }} 
+                        onDragOver={ (e) => handleDragOver(e) }
+                        onDrop={ (e) => handleUpdateGeneralFunction(e, functionsIndex, currentDraggedItem) }
                     >
-                        {f}
+                        <Typography variant='caption' color={"#CCCCCC"}>{ f }</Typography>
                     </Card>
+                </Grid>
+                <button 
+                    className={styles.confirm}
+                    onClick={() => handleConfirmFunction()}
+                >
+                    Confirm
+                </button>
+                <Grid 
+                    sx={{ ...gridItemStyle, border: 'none', boxShadow: 'none', mt: '2rem' }} 
+                    xs={ 12 } 
+                    item 
+                    className='available-functions'
+                >
+                    <Box>Function area</Box>
                 </Grid>
             </Grid>
         </Box>
