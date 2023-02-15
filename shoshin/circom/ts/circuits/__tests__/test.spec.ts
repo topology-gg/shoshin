b1c41c09bbd41d2e82d4047aea8c29f4a619cb6c
Lev Stambler <levstamb@gmail.com>
2023-02-15 16:43:43 -0500

    bug with comparator

--- shoshin/circom/ts/circuits/__tests__/test.spec.ts
+++ shoshin/circom/ts/circuits/__tests__/test.spec.ts
@@ -7,19 +7,47 @@ import { wasm as wasm_tester } from 'circom_tester';
 //@ts-ignore
 import { F1Field, Scalar } from 'ffjavascript';
 
+//@ts-ignore
+import { buildBabyjub } from 'circomlibjs';
+
+function getBits(v: Scalar, n = 254) {
+  const res = [];
+  for (let i = 0; i < n; i++) {
+    if (Scalar.isOdd(Scalar.shr(v, i))) {
+      res.push(Fr.one);
+    } else {
+      res.push(Fr.zero);
+    }
+  }
+  return res;
+}
+
 const p = Scalar.fromString(
   '21888242871839275222246405745257275088548364400416034343698204186575808495617'
 );
 const Fr = new F1Field(p);
 
-// TODO: put into a common lib... also means we hae to convert this function to ts
-
 const load_FD_circuit = async () => {
   const circuit = await wasm_tester(
     path.join(__dirname, 'circuits', 'fd_tester.circom')
   );
+  await circuit.loadSymbols();
   await circuit.loadConstraints();
   return circuit;
+};
+
+const mapInputToBN = (inp: any) => {
+  const toObj = (a: any) => BigInt(a);
+  const arrToObj = (a: any[]) => a.map(toObj);
+  return {
+    next_state: inp.next_state.map(toObj),
+    inputs: inp.inputs.map(toObj),
+    conditional_mux_sel: inp.conditional_mux_sel.map(arrToObj),
+    conditional_inputs_mux_sel: inp.conditional_inputs_mux_sel.map(arrToObj),
+    buffer_mux_sel: inp.buffer_mux_sel.map(arrToObj),
+    buffer_type_sel: inp.buffer_type_sel.map(arrToObj),
+    and_selectors: inp.and_selectors.map(arrToObj),
+  };
 };
 
 describe('FD VM Circuit test', () => {
@@ -67,13 +95,99 @@ describe('FD VM Circuit test', () => {
     circuit.assertOut(witness, { selected_next: Fr.e(3) });
   });
 
-  xit('Test default return upon condition not met', async () => {
+  it.only('Test abs valuing', async () => {
     const circuit = await load_FD_circuit();
+    const babyJub = await buildBabyjub();
+    const inps = ['-10', '10', '30', '40', '50'];
+    console.log('INPUTS', inps);
+    // 2 buffers, 5 inputs, 5 conditionals, 32 size words
+    const INPUT = {
+      next_state: [1, 2, 3, 4, 5, 6], // Match to each conditional
+      inputs: inps,
+      conditional_mux_sel: [
+        [0, 0], // ==
+        [0, 0],
+        [1, 0],
+        [1, 1],
+        [0, 1],
+      ],
+      conditional_inputs_mux_sel: [
+        [5, 1], // |-10| == 10
+        [2, 3], // 30 == 40
+        [3, 4], // 40 \leq 50 (Checks out!)
+        [4, 5], // 50 != 230
+        [5, 6], // 230 < 950 ],
+      ],
+      buffer_mux_sel: [
+        [0, 2, 0], // calculate |-10|
+        [2, 2, 4], // Calculate 30 * 30 + 50
+        [2, 2, 4], // Calculate 30 * 30 + 50 // TODO: NOTE BUFFER INPUTS CANNOT BE 0
+      ],
+      buffer_type_sel: [
+        [1, 0], // absolute value of -10
+        [0, 0],
+        [0, 0],
+      ],
+      // Bypass all ands
+      and_selectors: [
+        [0, 5, 5], // 6 is the false preset
+        [1, 5, 5],
+        [2, 5, 5],
+        [3, 5, 5],
+        [4, 5, 5],
+      ],
+    };
+
+    const witness = await circuit.calculateWitness(INPUT, true);
+    circuit.assertOut(witness, { selected_next: Fr.e(1) });
   });
 
-  it('Test Andind', async () => {
+  it('Test default return upon condition not met', async () => {
     const circuit = await load_FD_circuit();
-    // TODO: !!!! I DO NOT HAVE AND OR OR's as operations yet...
+    // 2 buffers, 5 inputs, 5 conditionals, 32 size words
+    const INPUT = {
+      next_state: [1, 2, 3, 4, 5, 6], // Match to each conditional
+      inputs: [10, 20, 30, 40, 50],
+      conditional_mux_sel: [
+        [0, 1],
+        [0, 0],
+        [1, 0],
+        [1, 1],
+        [0, 1],
+      ],
+      conditional_inputs_mux_sel: [
+        [2, 1], // 30 < 20
+        [2, 3], // 30 == 40
+        [3, 4], // 40 \leq 50 (Checks out!)
+        [4, 5], // 50 != 230
+        [5, 6], // 230 < 950 ],
+      ],
+      buffer_mux_sel: [
+        [0, 1, 2], // Calculate 10 * 20 + 30
+        [2, 2, 4], // Calculate 30 * 30 + 50
+        [2, 2, 4], // Calculate 30 * 30 + 50
+      ],
+      buffer_type_sel: [
+        [0, 0],
+        [0, 0],
+        [0, 0],
+      ],
+      // Bypass all ands
+      and_selectors: [
+        [0, 5, 6], // 6 is the false preset
+        [1, 5, 6],
+        [2, 5, 6],
+        [3, 5, 6],
+        [4, 5, 6],
+      ],
+    };
+
+    const witness = await circuit.calculateWitness(INPUT, true);
+    circuit.assertOut(witness, { selected_next: Fr.e(6) });
+  });
+
+  it('Test And-ing different conditionals', async () => {
+    const circuit = await load_FD_circuit();
     const INPUT = {
       next_state: [1, 2, 21, 211, 2111, 5], // Match to each conditional
       inputs: [1, 21, 5, 40, 50],
@@ -115,7 +229,6 @@ describe('FD VM Circuit test', () => {
 
   it('Test buffer divide mod', async () => {
     const circuit = await load_FD_circuit();
-    // TODO: !!!! I DO NOT HAVE AND OR OR's as operations yet...
     const INPUT = {
       next_state: [1, 2, 21, 211, 2111, 5], // Match to each conditional
       inputs: [1, 21, 5, 40, 50],
@@ -139,7 +252,7 @@ describe('FD VM Circuit test', () => {
         [2, 5, 6], // Calculate (a//b) * b + r
       ],
       buffer_type_sel: [
-        [0, 1], // a // b // TODO: NOTE DOWN THE REVERSE ORDER!!!! I.e. this is 2
+        [0, 1], // a // b
         [1, 1], // a % b
         [0, 0], // r1cs
       ],
