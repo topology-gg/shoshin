7a5d3c35694b65e25558247316c872f5defea0e8
Lev Stambler <levstamb@gmail.com>
2023-02-23 20:32:35 -0500

    progress with comments

--- circom/src/__tests__/unit/ts_evalutor.spec.ts
+++ circom/src/__tests__/unit/ts_evalutor.spec.ts
@@ -7,20 +7,20 @@ import {
 
 describe('TypeScript dag evaluation', () => {
   it('should evaluate a simple dag and give the output value', () => {
-    const out = ts_dag_evaluator(dag_simple);
+    const out = ts_dag_evaluator(dag_simple, []);
     expect(out).toEqual(BigInt(0));
   });
 
   it('should evaluate a simple arithmetic DAG', () => {
-    const out = ts_dag_evaluator(dag_arithmetic);
+    const out = ts_dag_evaluator(dag_arithmetic, []);
     expect(out).toEqual(BigInt(9));
   });
 
   it('should evaluate a simple non tree arithmetic DAG', () => {
-    const out = ts_dag_evaluator(dag_arithmetic_with_memo);
+    const out = ts_dag_evaluator(dag_arithmetic_with_memo, []);
     console.log(`OUT OF ${out}`);
     expect(out).toEqual(BigInt(7));
   });
 
-  xit('should throw an error when the maximum number of buffers are exceeded', () => {});
+  xit('should evaluate a tree with a dict', () => {});
 });
