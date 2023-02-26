e2ba301238ddb867f5fb86f5a826f482ecffa287
Lev Stambler <levstamb@gmail.com>
2023-02-26 09:57:40 -0500

    getting pr 1 ready

--- shoshin/circom/circuits/__tests__/circuits/fd_wrapper_tester.circom
+++
@@ -1,5 +0,0 @@
-pragma circom 2.0.0;
-include "../../FDWrapper.circom";
-
-// 32 traces, 5 public inputs, 10 FD constants, 32 conditionals, 32 bit words, and max and size of 5
-component main = FD_Emulator(32, 5, 10, 32, 32, 5);
--- shoshin/circom/circuits/__tests__/fd_emulator.spec.ts
+++ shoshin/circom/circuits/__tests__/fd_emulator.spec.ts
@@ -36,204 +36,100 @@ const load_FD_circuit = async () => {
   return circuit;
 };
 
-enum OpCodes {
-  ABS = 0,
-  INTEGER_DIV = 1,
-  ADD = 2,
+export enum OpCodes {
+  ADD = 1,
+  SUB = 2,
   MUL = 3,
-  EQ = 4,
-  LT = 5,
-  LTE = 6,
-  OR = 7,
+  DIV = 4,
+  // MOD = 5,
+  ABS = 6,
+  // SQRT = 7,
+  // POW = 8,
+
+  IS_NN = 9,
+  IS_LE = 10,
+  NOT = 11,
+  EQ = 12,
+
+  // MEM = 13,
+  DICT = 14,
+  // FUNC = 15,
 }
 
 describe('FD Emulator Circuit test', () => {
-  it.only('Test basic FD function', async () => {
+  it.only("Test basic FD function for sanity's sake", async () => {
     const circuit = await load_FD_circuit();
     // Compute abs(-10) + (30 / -10) = 7
     // 3 traces, 6 inputs
     const INPUT = {
-      inputs: [-10, 20, 30, 40, 50, 60],
+      inputs: [-10, 20, 30, 40, 50, 60, 0, 0],
       trace_inp_selectors: [
         [0, 1],
         [2, 0],
-        [6, 7],
+        [8, 9],
+        [10, 0],
+        [11, 0],
+        [12, 0],
+        [13, 0],
+        [14, 0],
+        [15, 0],
+        [16, 0],
+        [17, 0],
+        [18, 0],
+        [19, 0],
+        [20, 0],
+        [21, 0],
+        [22, 0],
+        [23, 0],
+        [24, 0],
+        [25, 0],
+        [26, 0],
+        [27, 0],
+        [28, 0],
+        [29, 0],
+        [30, 0],
+        [31, 0],
+        [32, 0],
+        [33, 0],
+        [34, 0],
+        [35, 0],
+        [36, 0],
       ],
-      trace_type_selectors: [OpCodes.ABS, OpCodes.INTEGER_DIV, OpCodes.ADD],
+      trace_type_selectors: [
+        OpCodes.ABS,
+        OpCodes.DIV,
+        OpCodes.ADD,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+      ],
     };
 
     const witness = await circuit.calculateWitness(INPUT, true);
     circuit.assertOut(witness, { out: Fr.e(7) });
-  });
-
-  xit('Test abs valuing', async () => {
-    const circuit = await load_FD_circuit();
-    const inps = ['-10', '10', '30', '40', '50'];
-    // 2 traces, 5 inputs, 5 conditionals, 32 size words
-    const INPUT = {
-      next_state: [1, 2, 3, 4, 5, 6], // Match to each conditional
-      inputs: inps,
-      conditional_mux_sel: [
-        [0, 0], // ==
-        [0, 0],
-        [1, 0],
-        [1, 1],
-        [0, 1],
-      ],
-      conditional_inputs_mux_sel: [
-        [5, 1], // |-10| == 10
-        [2, 3], // 30 == 40
-        [3, 4], // 40 \leq 50 (Checks out!)
-        [4, 5], // 50 != 230
-        [5, 6], // 230 < 950 ],
-      ],
-      trace_mux_sel: [
-        [0, 2, 0], // calculate |-10|
-        [2, 2, 4], // Calculate 30 * 30 + 50
-        [2, 2, 4], // Calculate 30 * 30 + 50 // TODO: NOTE TRACE INPUTS CANNOT BE 0
-      ],
-      trace_type_sel: [
-        [1, 0], // absolute value of -10
-        [0, 0],
-        [0, 0],
-      ],
-      // Bypass all ands
-      and_selectors: [
-        [0, 5, 5], // 6 is the false preset
-        [1, 5, 5],
-        [2, 5, 5],
-        [3, 5, 5],
-        [4, 5, 5],
-      ],
-    };
-
-    const witness = await circuit.calculateWitness(INPUT, true);
-    circuit.assertOut(witness, { selected_next: Fr.e(1) });
-  });
-
-  xit('Test default return upon condition not met', async () => {
-    const circuit = await load_FD_circuit();
-    // 2 traces, 5 inputs, 5 conditionals, 32 size words
-    const INPUT = {
-      next_state: [1, 2, 3, 4, 5, 6], // Match to each conditional
-      inputs: [10, 20, 30, 40, 50],
-      conditional_mux_sel: [
-        [0, 1],
-        [0, 0],
-        [1, 0],
-        [1, 1],
-        [0, 1],
-      ],
-      conditional_inputs_mux_sel: [
-        [2, 1], // 30 < 20
-        [2, 3], // 30 == 40
-        [3, 4], // 40 \leq 50 (Checks out!)
-        [4, 5], // 50 != 230
-        [5, 6], // 230 < 950 ],
-      ],
-      trace_mux_sel: [
-        [0, 1, 2], // Calculate 10 * 20 + 30
-        [2, 2, 4], // Calculate 30 * 30 + 50
-        [2, 2, 4], // Calculate 30 * 30 + 50
-      ],
-      trace_type_sel: [
-        [0, 0],
-        [0, 0],
-        [0, 0],
-      ],
-      // Bypass all ands
-      and_selectors: [
-        [0, 5, 6], // 6 is the false preset
-        [1, 5, 6],
-        [2, 5, 6],
-        [3, 5, 6],
-        [4, 5, 6],
-      ],
-    };
-
-    const witness = await circuit.calculateWitness(INPUT, true);
-    circuit.assertOut(witness, { selected_next: Fr.e(6) });
-  });
-
-  xit('Test And-ing different conditionals', async () => {
-    const circuit = await load_FD_circuit();
-    const INPUT = {
-      next_state: [1, 2, 21, 211, 2111, 5], // Match to each conditional
-      inputs: [1, 21, 5, 40, 50],
-      conditional_mux_sel: [
-        [0, 0], // ==
-        [0, 0], // ==
-        [1, 0], // <
-        [1, 1], // !=
-        [0, 1], // <=
-      ],
-      conditional_inputs_mux_sel: [
-        [1, 7], // check that `trace 3` == `input 1`
-        [2, 3], // 21 ?== 21 (false)
-        [3, 4], // 211 < 2111 (true)
-        [4, 5], // 2111 != (21 // 5)
-        [5, 6], // dummy...
-      ],
-      trace_mux_sel: [
-        [1, 2, 0], // Calculate 21 // 5 = 4
-        [1, 2, 0], // Calculate 21 % 5 = 1
-        [2, 5, 6], // Calculate (a//b) * b + r
-      ],
-      trace_type_sel: [
-        [0, 1], // a // b // TODO: NOTE DOWN THE REVERSE ORDER!!!! I.e. this is 2
-        [1, 1], // a % b
-        [0, 0], // r1cs
-      ],
-      and_selectors: [
-        [0, 1, 5], // and 0 and 1, should be false
-        [0, 2, 3], // should be true
-        [2, 5, 5],
-        [3, 5, 5],
-        [4, 5, 5],
-      ],
-    };
-    const witness = await circuit.calculateWitness(INPUT, true);
-    circuit.assertOut(witness, { selected_next: Fr.e(2) });
-  });
-
-  xit('Test trace divide mod', async () => {
-    const circuit = await load_FD_circuit();
-    const INPUT = {
-      next_state: [1, 2, 21, 211, 2111, 5], // Match to each conditional
-      inputs: [1, 21, 5, 40, 50],
-      conditional_mux_sel: [
-        [0, 0], // ==
-        [0, 0], // ==
-        [1, 0], // <
-        [1, 1], // !=
-        [0, 1], // <=
-      ],
-      conditional_inputs_mux_sel: [
-        [1, 7], // check that `trace 3` == `input 1`
-        [2, 3], // dummy...
-        [3, 4], // dummy...
-        [4, 5], // dummy...
-        [5, 6], // dummy...
-      ],
-      trace_mux_sel: [
-        [1, 2, 0], // Calculate 21 // 5 = 4
-        [1, 2, 0], // Calculate 21 % 5 = 1
-        [2, 5, 6], // Calculate (a//b) * b + r
-      ],
-      trace_type_sel: [
-        [0, 1], // a // b
-        [1, 1], // a % b
-        [0, 0], // r1cs
-      ],
-      and_selectors: [
-        [0, 5, 5],
-        [1, 5, 5],
-        [2, 5, 5],
-        [3, 5, 5],
-        [4, 5, 5],
-      ],
-    };
-    const witness = await circuit.calculateWitness(INPUT, true);
-    circuit.assertOut(witness, { selected_next: Fr.e(1) });
   });
 });
