bee9754cb34eb3d52a58b9c44e6539ab8175f81a
Gregory Edison <gregory.edison1993@gmail.com>
2023-03-02 14:30:36 +0100

    test: add testing

---
+++ tooling/src/test/leaf.test.ts
@@ -0,0 +1,18 @@
+import "mocha";
+import { expect } from "chai";
+import Leaf, { SimpleLeaf, flattenLeaf, unflattenLeaf } from "../types/Leaf";
+
+
+describe("flatten", () => {
+    describe("leaf", () => {
+        it("should flatten the leaf to the correct value", () => {
+        });
+    });
+});
+
+describe("unflatten", () => {
+    describe("simple leaf", () => {
+        it("should unflatten the simple leaf to the correct value", () => {
+        });
+    });
+});
