be9ae67c05067851c7c2da45fb99c7cd16f58a99
Gregory Edison <gregory.edison1993@gmail.com>
2023-03-03 14:02:41 +0100

    chore: adversary selection reorg

--- tooling/src/hooks/useRunCairoSimulation.ts
+++ tooling/src/hooks/useRunCairoSimulation.ts
@@ -1,59 +1,15 @@
 import { useCallback, useContext, useState } from "react";
-import {
-    DECISION_TREE_COMBO_AGENT,
-    DEFENSIVE_AGENT,
-    INITIAL_FUNCTIONS,
-    MENTAL_STATES_COMBO_AGENT,
-    OFFENSIVE_AGENT,
-} from "../constants/constants";
 import { WASMContext } from "../context/WASM";
 import cairoOutputToFrameScene from "../helpers/cairoOutputToFrameScene";
-import Agent, { buildAgent, flattenAgent } from "../types/Agent";
+import Agent, { flattenAgent } from "../types/Agent";
 import { FrameScene } from "../types/Frame";
-
-const getDummyDefensiveArgs = () => {
-    let agent = DEFENSIVE_AGENT;
-    return [...flattenAgent(agent), new Int32Array(agent.actions)];
-};
-
-const getDummyOffensiveArgs = () => {
-    let agent = OFFENSIVE_AGENT;
-    return [...flattenAgent(agent), new Int32Array(agent.actions)];
-};
-
-const getDummyComboArgs = (combo: number[]) => {
-    let agent: Agent = buildAgent(
-        MENTAL_STATES_COMBO_AGENT,
-        [combo],
-        DECISION_TREE_COMBO_AGENT,
-        INITIAL_FUNCTIONS,
-        0,
-        1
-    );
-    console.log(agent);
-    return [...flattenAgent(agent), new Int32Array(agent.actions)];
-};
-
-const getDummyArgs = (type: string, combo: number[]) => {
-    switch (type) {
-        case "defensive": {
-            return getDummyDefensiveArgs();
-        }
-        case "offensive": {
-            return getDummyOffensiveArgs();
-        }
-        case "combo": {
-            return getDummyComboArgs(combo);
-        }
-    }
-};
 
 /**
  * Hook to run the Cairo simulation using provided agent and adversary
  */
 const useRunCairoSimulation = (
     agent: Agent,
-    adversary: string,
+    opponent: Agent,
     combo: number[]
 ) => {
     const ctx = useContext(WASMContext);
@@ -77,14 +33,13 @@ const useRunCairoSimulation = (
         ] = flattenAgent(agent);
         // flatten the dummy agent
         let [
-            dummyCombosOffset,
-            dummyCombos,
-            dummyMentalStatesOffset,
-            dummyMentalStates,
-            dummyFunctionsOffset,
-            dummyFunctions,
-            dummyActions,
-        ] = getDummyArgs(adversary, combo);
+            opponentCombosOffset,
+            opponentCombos,
+            opponentMentalStatesOffset,
+            opponentMentalStates,
+            opponentFunctionsOffset,
+            opponentFunctions,
+        ] = flattenAgent(opponent);
 
         try {
             let shoshinInput = new Int32Array([
@@ -92,34 +47,34 @@ const useRunCairoSimulation = (
                 ...combosOffset,
                 combos.length,
                 ...combos,
-                dummyCombosOffset.length,
-                ...dummyCombosOffset,
-                dummyCombos.length,
-                ...dummyCombos,
+                opponentCombosOffset.length,
+                ...opponentCombosOffset,
+                opponentCombos.length,
+                ...opponentCombos,
                 mentalStatesOffset.length,
                 ...mentalStatesOffset,
                 mentalStates.length / 3,
                 ...mentalStates,
                 agent.initialState,
-                dummyMentalStatesOffset.length,
-                ...dummyMentalStatesOffset,
-                dummyMentalStates.length / 3,
-                ...dummyMentalStates,
+                opponentMentalStatesOffset.length,
+                ...opponentMentalStatesOffset,
+                opponentMentalStates.length / 3,
+                ...opponentMentalStates,
                 0,
                 functionsOffset.length,
                 ...functionsOffset,
                 functions.length / 3,
                 ...functions,
-                dummyFunctionsOffset.length,
-                ...dummyFunctionsOffset,
-                dummyFunctions.length / 3,
-                ...dummyFunctions,
+                opponentFunctionsOffset.length,
+                ...opponentFunctionsOffset,
+                opponentFunctions.length / 3,
+                ...opponentFunctions,
                 agent.actions.length,
                 ...agent.actions,
-                dummyActions.length,
-                ...dummyActions,
+                opponent.actions.length,
+                ...opponent.actions,
                 agent.character,
-                1,
+                opponent.character,
             ]);
             let output = ctx.wasm.runCairoProgram(shoshinInput);
             setOutput(cairoOutputToFrameScene(output));
@@ -127,7 +82,7 @@ const useRunCairoSimulation = (
             console.log("Got an error running wasm", e);
             setError(e);
         }
-    }, [ctx, agent, adversary, combo]);
+    }, [ctx, agent, opponent, combo]);
 
     return {
         wasmReady: ctx.wasm,
