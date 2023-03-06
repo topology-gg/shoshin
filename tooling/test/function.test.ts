ce4b997b2160857093ca7b3f36e1090326171280
Gregory Edison <gregory.edison1993@gmail.com>
2023-03-06 16:05:32 +0100

    test: update function parsing

---
+++ tooling/test/function.test.ts
@@ -0,0 +1,160 @@
+import "mocha"
+import { expect } from "chai"
+import { Operator, ElementType, Function, parseFunction, Perceptible } from '../src/types/Function'
+
+describe("parse", () => {
+    describe("function", () => {
+        it("should parse the OR functions to the expected result", () => {
+            // Given
+            let f: Function = {
+                elements: [
+                    { value: Operator.OpenParenthesis, type: ElementType.Operator },
+                    { value: Perceptible.OpponentBodyState, type: ElementType.Perceptible },
+                    { value: Operator.Equal, type: ElementType.Operator },
+                    { value: 10, type: ElementType.Constant },
+                    { value: Operator.CloseParenthesis, type: ElementType.Operator },
+                    { value: Operator.Or, type: ElementType.Operator },
+                    { value: Operator.OpenParenthesis, type: ElementType.Operator },
+                    { value: Perceptible.OpponentBodyState, type: ElementType.Perceptible },
+                    { value: Operator.Equal, type: ElementType.Operator },
+                    { value: 20, type: ElementType.Constant },
+                    { value: Operator.CloseParenthesis, type: ElementType.Operator },
+                    { value: Operator.Or, type: ElementType.Operator },
+                    { value: Operator.OpenParenthesis, type: ElementType.Operator },
+                    { value: Perceptible.OpponentBodyState, type: ElementType.Perceptible },
+                    { value: Operator.Equal, type: ElementType.Operator },
+                    { value: 30, type: ElementType.Constant },
+                    { value: Operator.CloseParenthesis, type: ElementType.Operator },
+                ]
+            }
+            // When 
+            let got = parseFunction(f)
+            // Then
+            let expected = {
+                value: 1, 
+                left: {
+                    value: 12, 
+                    left: {value: 14, left: -1, right: {value: 110, left: -1, right: -1}}, 
+                    right: {value: 10, left: -1, right: -1},
+                },
+                right: {
+                    value: 1, 
+                    left: {
+                        value: 12, 
+                        left: {value: 14, left: -1, right: {value: 110, left: -1, right: -1}}, 
+                        right: {value: 20, left: -1, right: -1},
+                    },
+                    right: {
+                        value: 12, 
+                        left: {value: 14, left: -1, right: {value: 110, left: -1, right: -1}}, 
+                        right: {value: 30, left: -1, right: -1},
+                    }
+                }
+            }
+            expect(got).deep.equal(expected)
+        });
+        it("should parse the ABS function to the expected value", ()=> {
+            // Given
+            let f: Function = {
+                elements: [
+                    { value: Operator.OpenAbs, type: ElementType.Operator},
+                    { value: Perceptible.SelfX, type: ElementType.Perceptible},
+                    { value: Operator.Sub, type: ElementType.Operator},
+                    { value: Perceptible.OpponentX, type: ElementType.Perceptible},
+                    { value: Operator.CloseAbs, type: ElementType.Operator},
+                    { value: Operator.Lte, type: ElementType.Operator},
+                    { value: 80, type: ElementType.Constant},
+                ]
+            }
+            // When 
+            let got = parseFunction(f)
+            // Then
+            let expected = {
+                value: 10,
+                left: {
+                    value: 6, 
+                    left: -1,
+                    right: {
+                        value: 2, 
+                        left: {value: 14, left: -1, right: {value: 1, left: -1, right: -1}}, 
+                        right: {value: 14, left: -1, right: {value: 101, left: -1, right: -1}}, 
+                    }
+                },
+                right: {value: 80, left: -1, right: -1},
+            }
+            expect(got).deep.equal(expected)
+        });
+        it("should parse the single ABS function to the correct value", () => {
+            // Given
+            let f: Function = {
+                elements: [
+                    { value: Operator.OpenAbs, type: ElementType.Operator},
+                    { value: Perceptible.OpponentVelX, type: ElementType.Perceptible},
+                    { value: Operator.CloseAbs, type: ElementType.Operator},
+                    { value: Operator.Lte, type: ElementType.Operator},
+                    { value: 10, type: ElementType.Constant},
+                ]
+            }
+            // When 
+            let got = parseFunction(f)
+            // Then
+            let expected = {
+                value: 10,
+                left: {
+                    value: 6, 
+                    left: -1,
+                    right: {value: 14, left: -1, right: {value: 103, left: -1, right: -1}}, 
+                },
+                right: {value: 10, left: -1, right: -1},
+            }
+            expect(got).deep.equal(expected)
+        })
+        it("should parse the combined ABS and AND functions to the correct value", () => {
+            // Given
+            let f: Function = {
+                elements: [
+                    { value: Operator.OpenParenthesis, type: ElementType.Operator},
+                    { value: Perceptible.OpponentInt, type: ElementType.Perceptible},
+                    { value: Operator.Lte, type: ElementType.Operator},
+                    { value: 300, type: ElementType.Constant},
+                    { value: Operator.CloseParenthesis, type: ElementType.Operator},
+                    { value: Operator.And, type: ElementType.Operator},
+                    { value: Operator.OpenParenthesis, type: ElementType.Operator},
+                    { value: Operator.OpenAbs, type: ElementType.Operator},
+                    { value: Perceptible.SelfX, type: ElementType.Perceptible},
+                    { value: Operator.Sub, type: ElementType.Operator},
+                    { value: Perceptible.OpponentX, type: ElementType.Perceptible},
+                    { value: Operator.CloseAbs, type: ElementType.Operator},
+                    { value: Operator.Lte, type: ElementType.Operator},
+                    { value: 80, type: ElementType.Constant},
+                    { value: Operator.CloseParenthesis, type: ElementType.Operator},
+                ]
+            }
+            // When
+            let got = parseFunction(f)
+            // Then
+            let expected = {
+                value: 3, 
+                left: {
+                    value: 10, 
+                    left: {value: 14, left: -1, right: {value: 108, left: -1, right: -1}}, 
+                    right: {value: 300, left: -1, right: -1},
+                },
+                right: {
+                    value: 10,
+                    left: {
+                        value: 6, 
+                        left: -1,
+                        right: {
+                            value: 2, 
+                            left: {value: 14, left: -1, right: {value: 1, left: -1, right: -1}}, 
+                            right: {value: 14, left: -1, right: {value: 101, left: -1, right: -1}}, 
+                        }
+                    },
+                    right: {value: 80, left: -1, right: -1},
+                }
+            }
+            expect(got).deep.equal(expected)
+        })
+    });
+});
