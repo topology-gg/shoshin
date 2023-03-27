e701d6ab7b1be145c8f91d74c21715803fed7995
Matthew Demoy <0xNonCents@gmail.com>
2023-03-27 11:49:13 -0400

    game and simulator

--- tooling/src/Game/PhaserGame.tsx
+++ tooling/src/Game/PhaserGame.tsx
@@ -1,113 +1,167 @@
 /**
  * @file Game.js
  */
-import * as React from 'react';
-import PropTypes from 'prop-types';
+import * as React from "react";
+import PropTypes from "prop-types";
 
-import { useLayoutEffect } from '../hooks/useIsomorphicLayoutEffect';
-import styles from './Game.module.css';
+import { useLayoutEffect } from "../hooks/useIsomorphicLayoutEffect";
+import styles from "./Game.module.css";
+import { TestJson } from "../types/Frame";
+import Simulator from "../scene/Simulator";
 
-const Game = ({
-  tagName: Tag = 'div',
-  className = 'relative top-0 left-0 w-full h-full my-12',
-  variant = 'default',
-}) => {
-  const parent = React.useRef();
-  const canvas = React.useRef();
-  const game = React.useRef();
-  const Phaser = React.useMemo(() => {
-    if (
-      typeof window !== 'undefined' &&
-      typeof window.navigator !== 'undefined'
-    ) {
-      return require('phaser');
-    }
-  }, []);
+interface SimulatorProps {
+    testJson: TestJson;
+    animationFrame: number;
+    showDebug: boolean;
+}
+
+const Game = ({testJson, animationFrame, showDebug}: SimulatorProps) => {
+    const tagName = "div";
+    const className = "relative top-0 left-0 w-full h-full my-12";
+    const variant = "default";
+
+    const parent = React.useRef();
+    const canvas = React.useRef();
+    const game = React.useRef();
+    const Phaser = React.useMemo(() => {
+        if (
+            typeof window !== "undefined" &&
+            typeof window.navigator !== "undefined"
+        ) {
+            return require("phaser");
+        }
+    }, []);
+
+    // import('phaser/src/phaser').then((mod) => {
+    //   if (mod && !Phaser) {
+    //     setPhaser(mod.default)
+    //   }
+    // })
+
+    const preload = React.useCallback(() => {
+        const g = game.current;
+        const _this = g.scene.keys.default;
+        console.log("preload ->  preloading assets...", _this);
+        //_this.load.setBaseURL('http://labs.phaser.io');
+        _this.load.image("sky", "images/bg/shoshin-bg-white-long.png");
+    }, []);
 
 
-  // import('phaser/src/phaser').then((mod) => {
-  //   if (mod && !Phaser) {
-  //     setPhaser(mod.default)
-  //   }
-  // })
+    const characterType0 = testJson?.agent_0.type
+    const characterType1 = testJson?.agent_1.type
+    const agentFrame0 = testJson?.agent_0.frames[animationFrame]
+    const agentFrame1 = testJson?.agent_1.frames[animationFrame]
 
-  const preload = React.useCallback(() => {
-    const g = game.current;
-    const _this = g.scene.keys.default;
-    console.log('preload ->  preloading assets...', _this);
-    //_this.load.setBaseURL('http://labs.phaser.io');
-    _this.load.image('sky', 'images/bg/shoshin-bg-white-long.png');
+    const characterName0 = characterType0 == 0 ? 'jessica' : 'antoc'
+    const characterName1 = characterType1 == 0 ? 'jessica' : 'antoc'
+
+
+    const create = React.useCallback((e) => {
+        const g = game.current;
+        const _this = g.scene.keys.default;
+
+        console.log("create -> creating elements...", _this);
+        _this.add.image(400, 300, "sky");
+
+        var particles = _this.add.particles("red");
+
+        var emitter = particles.createEmitter({
+            speed: 100,
+            scale: { start: 1, end: 0 },
+            blendMode: "ADD",
+        });
+
+        var logo = _this.physics.add.image(400, 100, "logo");
+
+        logo.setVelocity(100, 200);
+        logo.setBounce(1, 1);
+        logo.setCollideWorldBounds(true);
+
+        emitter.startFollow(logo);
+    }, []);
+
+    useLayoutEffect(() => {
+        let g;
+        if (Phaser && parent.current && canvas.current) {
+            const config = {
+                type: Phaser.CANVAS,
+                parent: parent.current,
+                canvas: canvas.current,
+                width: 800,
+                height: 600,
+                // pixelArt: true,
+                autoCenter: true,
+                backgroundColor: "#000000",
+                physics: {
+                    default: "arcade",
+                    arcade: {
+                        gravity: { y: 200 },
+                    },
+                },
+                scene: {
+                },
+                // callbacks: {
+                //   preBoot: (game) => console.log('preBoot -> ', game),
+                //   postBoot: (game) => console.log('postBoot -> ', game),
+                // }
+            };
+            g = game.current = new Phaser.Game(config);
+
+            g.scene.add('simulator', Simulator)
+            g.scene.start('simulator')
+            console.log("game", game.current);
+        }
+        return () => g.destroy();
+    }, [Phaser, create, preload, parent, canvas]);
+
+
+
+    React.useEffect(() => {
+        //get current scene
+
+        if(game == undefined)
+        {
+          return
+        }
+        let scene = (game.current.scene.getScene('simulator') as Simulator);
+
+        if(scene == undefined)
+        {
+          return
+        }
+        //get sprite one and two
+        const characterType0 = testJson?.agent_0.type
+        const characterType1 = testJson?.agent_1.type
+        const agentFrame0 = testJson?.agent_0.frames[animationFrame]
+        const agentFrame1 = testJson?.agent_1.frames[animationFrame]
+        scene.setPlayerOneCharacter(characterType0)
+        scene.setPlayerTwoCharacter(characterType1)
+        scene.setPlayerOneFrame(agentFrame0);
+        scene.setPlayerTwoFrame(agentFrame1);
+        scene.setPlayerOneBodyHitbox(agentFrame0)
+        scene.setPlayerTwoBodyHitbox(agentFrame1)
+        scene.setPlayerOneActionHitbox(agentFrame0)
+        scene.setPlayerTwoActionHitbox(agentFrame1)
+        //render stuff
+    }, [testJson, animationFrame, showDebug])
     
-  }, []);
-
-  const create = React.useCallback(e => {
-    const g = game.current;
-    const _this = g.scene.keys.default;
-    console.log('create -> creating elements...', _this);
-    _this.add.image(400, 300, 'sky');
-
-    var particles = _this.add.particles('red');
-
-    var emitter = particles.createEmitter({
-      speed: 100,
-      scale: { start: 1, end: 0 },
-      blendMode: 'ADD',
-    });
-
-    var logo = _this.physics.add.image(400, 100, 'logo');
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
-  }, []);
-
-  useLayoutEffect(() => {
-    let g;
-    if (Phaser && parent.current && canvas.current) {
-      const config = {
-        type: Phaser.CANVAS,
-        parent: parent.current,
-        canvas: canvas.current,
-        width: 800,
-        height: 600,
-        // pixelArt: true,
-        autoCenter: true,
-        backgroundColor: '#000000',
-        physics: {
-          default: 'arcade',
-          arcade: {
-            gravity: { y: 200 },
-          },
-        },
-        scene: {
-          preload: () => preload(),
-          create: e => create(e),
-        },
-        // callbacks: {
-        //   preBoot: (game) => console.log('preBoot -> ', game),
-        //   postBoot: (game) => console.log('postBoot -> ', game),
-        // }
-      };
-      g = game.current = new Phaser.Game(config);
-      console.log('game', game.current);
-    }
-    return () => g.destroy();
-  }, [Phaser, create, preload, parent, canvas]);
-  
-  return Phaser ? (
-    <Tag className={`${styles.game} ${styles[`game__${variant}`]} ${className}`} ref={parent}>
-      <canvas ref={canvas} />
-    </Tag>
-  ) : null;
+    return Phaser ? (
+        <div
+            className={`${styles.game} ${
+                styles[`game__${variant}`]
+            } ${className}`}
+            ref={parent}
+        >
+            <canvas ref={canvas} />
+        </div>
+    ) : null;
 };
 
 Game.propTypes = {
-  tagName: PropTypes.string,
-  className: PropTypes.string,
-  variant: PropTypes.oneOf(['default']),
-  children: PropTypes.node,
+    tagName: PropTypes.string,
+    className: PropTypes.string,
+    variant: PropTypes.oneOf(["default"]),
+    children: PropTypes.node,
 };
 
 export default Game;
---
+++ tooling/src/scene/Simulator.ts
@@ -0,0 +1,214 @@
+import Phaser from "phaser";
+import { bodyStateNumberToName } from "../constants/constants";
+import { Frame } from "../types/Frame";
+
+export default class Platformer extends Phaser.Scene {
+    private player_one : Phaser.GameObjects.Sprite;
+    private player_two : Phaser.GameObjects.Sprite;
+
+    private player_one_character : string;
+    private player_two_character : string;
+
+    private player_one_body_hitbox : Phaser.GameObjects.Rectangle;
+    private player_two_body_hitbox : Phaser.GameObjects.Rectangle;
+
+    private player_one_action_hitbox : Phaser.GameObjects.Rectangle;
+    private player_two_action_hitbox : Phaser.GameObjects.Rectangle;
+
+    private player_one_body_hitbox_text : Phaser.GameObjects.Text
+    private player_two_body_hitbox_text : Phaser.GameObjects.Text
+
+    private player_one_action_hitbox_text : Phaser.GameObjects.Text
+    private player_two_action_hitbox_text : Phaser.GameObjects.Text
+
+    preload(){
+        this.load.atlas(`antoc-idle`, 'images/antoc/idle/spritesheet.png',  'images/antoc/idle/spritesheet.json');
+        this.load.atlas(`antoc-walk_forward`, 'images/antoc/walk_forward/spritesheet.png',  'images/antoc/walk_forward/spritesheet.json');
+        this.load.atlas(`antoc-walk_backward`, 'images/antoc/walk_backward/spritesheet.png',  'images/antoc/walk_backward/spritesheet.json');
+        this.load.atlas(`antoc-hori`, 'images/antoc/hori/spritesheet.png',  'images/antoc/hori/spritesheet.json');
+        this.load.atlas(`antoc-hurt`, 'images/antoc/hurt/spritesheet.png',  'images/antoc/hurt/spritesheet.json');
+        this.load.atlas(`antoc-knocked`, 'images/antoc/knocked/spritesheet.png',  'images/antoc/knocked/spritesheet.json');
+        this.load.atlas(`antoc-vert`, 'images/antoc/vert/spritesheet.png',  'images/antoc/vert/spritesheet.json');
+        this.load.atlas(`antoc-dash_forward`, 'images/antoc/dash_forward/spritesheet.png',  'images/antoc/dash_forward/spritesheet.json');
+        this.load.atlas(`antoc-dash_backward`, 'images/antoc/dash_backward/spritesheet.png',  'images/antoc/dash_backward/spritesheet.json');
+        this.load.atlas(`antoc-block`, 'images/antoc/block/spritesheet.png',  'images/antoc/block/spritesheet.json');
+
+        this.load.atlas(`jessica-idle-right`, 'images/jessica/idle/right_spritesheet.png');
+        this.load.atlas(`jessica-idle-left`, 'images/jessica/idle/left_spritesheet.png');
+
+    }
+
+    create(){                
+        this.player_one = this.add.sprite(-100,0,`antoc-idle`, 0)
+        this.player_two = this.add.sprite(100,0,`antoc-idle`, 0)
+        this.player_two.setFlipX(true)
+        
+        this.player_one_body_hitbox = this.add.rectangle(0, 0, 0, 0)
+        this.player_one_body_hitbox.setStrokeStyle(2, 0xFCE205FF);
+
+        this.player_two_body_hitbox = this.add.rectangle(0, 0, 0, 0)
+        this.player_two_body_hitbox.setStrokeStyle(2, 0xFCE205FF);
+
+        this.player_one_action_hitbox = this.add.rectangle(0, 0, 0, 0)
+        this.player_one_action_hitbox.setStrokeStyle(2, 0xCC3333FF);
+
+        this.player_two_action_hitbox = this.add.rectangle(0, 0, 0, 0)
+        this.player_two_action_hitbox.setStrokeStyle(2, 0xCC3333FF);
+
+        this.player_one_body_hitbox_text = this.add.text(0,0,"")
+
+
+        //this.player_one_body_hitbox_text.setAlign('center')
+        this.player_one_body_hitbox_text.setFontSize(12)
+
+        
+        
+        this.player_two_body_hitbox_text = this.add.text(-100,-100,"")
+        this.player_two_body_hitbox_text.setFontSize(12)
+        //this.player_two_body_hitbox_text.setVisible(false)
+        this.player_one_action_hitbox_text = this.add.text(0,2,"")
+        this.player_one_action_hitbox_text.setFontSize(12)
+        //this.player_one_action_hitbox_text.setVisible(false)
+        this.player_two_action_hitbox_text = this.add.text(0,3,"")
+        this.player_two_action_hitbox_text.setFontSize(12)
+        //this.player_two_action_hitbox_text.setVisible(false)
+
+        
+        //Phaser.Display.Align.In.Center(this.player_one_action_hitbox_text, this.player_one_action_hitbox);
+        //Phaser.Display.Align.In.Center(this.player_two_action_hitbox_text, this.player_two_action_hitbox);
+
+
+        //this.cameras.main.startFollow(this.player_one_body_hitbox, true);
+        //this.cameras.main.setScroll(-300, -300)
+        this.cameras.main.centerOn(0, -200)
+
+        //this.cameras.main.setSize(this.cameras.main.width - 400, this.cameras.main.height - 400)
+    }
+    setPlayerOneCharacter(characterType : number){
+
+        const characterName = characterType == 0 ? 'jessica' : 'antoc'    
+        this.player_one_character = characterName
+        //this.player_one = this.add.sprite(0,0,`${characterName}-idle-right`)
+    }
+    setPlayerTwoCharacter(characterType : number){
+        const characterName = characterType == 0 ? 'jessica' : 'antoc'    
+        this.player_two_character = characterName
+        //this.player_two = this.add.sprite(0,0,`${characterName}-idle-left`)
+    }
+
+    setPlayerOneFrame(frame : Frame){
+        // Extract from frame
+        const bodyState = frame.body_state.state
+        const bodyStateCounter = frame.body_state.counter
+        const bodyStateDir = frame.body_state.dir
+        const physicsState = frame.physics_state
+        const pos = physicsState.pos
+
+        const characterName = this.player_one_character;
+
+        const bodyStateName = bodyStateNumberToName [characterName][bodyState]
+        const direction = (bodyStateDir == 1) ? 'right' : 'left'
+
+        this.player_one.setX(pos.x);
+        this.player_one.setY(pos.y);
+
+        this.player_one.setTexture(`${characterName}-${bodyStateName}`, `frame_${bodyStateCounter}.png`)
+        
+        
+    }
+
+    setPlayerTwoFrame(frame : Frame){
+        // Extract from frame
+        const bodyState = frame.body_state.state
+        const bodyStateCounter = frame.body_state.counter
+        const bodyStateDir = frame.body_state.dir
+        const physicsState = frame.physics_state
+        const pos = physicsState.pos
+
+        const characterName = this.player_two_character;
+
+        const bodyStateName = bodyStateNumberToName [characterName][bodyState]
+        const direction = (bodyStateDir == 1) ? 'right' : 'left'
+
+        this.player_two.setX(pos.x);
+        this.player_two.setY(pos.y);
+
+        this.player_two.setTexture(`${characterName}-${bodyStateName}`, bodyStateCounter)
+        
+    }
+
+    setPlayerOneBodyHitbox(frame : Frame) {
+        const hitbox = frame.hitboxes.body;
+        const hitboxW = hitbox.dimension.x
+        const hitboxH = hitbox.dimension.y
+        const hitboxX = hitbox.origin.x
+        const hitboxY = hitbox.origin.y
+
+
+        const centerX =  hitbox.origin.x + hitbox.dimension.x / 2
+        const centerY = hitbox.origin.y - hitbox.dimension.y / 2
+        //const left = viewWidth/2 + hitboxX
+        //const top = SIMULATOR_H - hitboxY - hitboxH + SpriteTopAdjustmentToBg
+
+        this.player_one_body_hitbox.setPosition(centerX, centerY)
+        this.player_one_body_hitbox.setSize(hitboxW, hitboxH)
+        this.player_one_body_hitbox_text.setText(`(${hitboxX},${hitboxY})\n${hitboxW}x${hitboxH}`)
+        Phaser.Display.Align.In.BottomCenter(this.player_one_body_hitbox_text, this.player_one_body_hitbox);
+        
+    }   
+
+    setPlayerTwoBodyHitbox(frame : Frame) {
+        const hitbox = frame.hitboxes.body;
+        const hitboxW = hitbox.dimension.x
+        const hitboxH = hitbox.dimension.y
+        const hitboxX = hitbox.origin.x
+        const hitboxY = hitbox.origin.y
+
+        const centerX =  hitbox.origin.x + hitbox.dimension.x / 2
+        const centerY = hitbox.origin.y - hitbox.dimension.y / 2
+        //const left = viewWidth/2 + hitboxX
+        //const top = SIMULATOR_H - hitboxY - hitboxH + SpriteTopAdjustmentToBg
+
+        this.player_two_body_hitbox.setPosition(centerX, centerY)
+        this.player_two_body_hitbox.setSize(hitboxW, hitboxH)
+        this.player_two_body_hitbox_text.setText(`(${hitboxX},${hitboxY})\n${hitboxW}x${hitboxH}`)
+        Phaser.Display.Align.In.BottomRight(this.player_two_body_hitbox_text, this.player_two_body_hitbox);
+    }   
+
+    setPlayerOneActionHitbox(frame : Frame) {
+        const hitbox = frame.hitboxes.action;
+        const hitboxW = hitbox.dimension.x
+        const hitboxH = hitbox.dimension.y
+        const hitboxX = hitbox.origin.x
+        const hitboxY = hitbox.origin.y
+
+        const centerX =  hitbox.origin.x + hitbox.dimension.x / 2
+        const centerY = - hitbox.origin.y - hitbox.dimension.y / 2
+        //const left = viewWidth/2 + hitboxX
+        //const top = SIMULATOR_H - hitboxY - hitboxH + SpriteTopAdjustmentToBg
+
+        this.player_one_action_hitbox.setPosition(centerX, centerY)
+        this.player_one_action_hitbox.setSize(hitboxW, hitboxH)
+        this.player_one_action_hitbox_text.setText(`(${hitboxX},${hitboxY})\n${hitboxW}x${hitboxH}`)
+        Phaser.Display.Align.In.BottomRight(this.player_one_action_hitbox_text, this.player_one_action_hitbox);
+    }   
+
+    setPlayerTwoActionHitbox(frame : Frame) {
+        const hitbox = frame.hitboxes.action;
+        const hitboxW = hitbox.dimension.x
+        const hitboxH = hitbox.dimension.y
+        const hitboxX = hitbox.origin.x
+        const hitboxY = hitbox.origin.y
+
+        const centerX =  hitbox.origin.x + hitbox.dimension.x / 2
+        const centerY = - hitbox.origin.y - hitbox.dimension.y / 2
+        //const left = viewWidth/2 + hitboxX
+        //const top = SIMULATOR_H - hitboxY - hitboxH + SpriteTopAdjustmentToBg
+
+        this.player_two_action_hitbox.setPosition(centerX, centerY)
+        this.player_two_action_hitbox.setSize(hitboxW, hitboxH)
+        this.player_two_action_hitbox_text.setText(`(${hitboxX},${hitboxY})\n${hitboxW}x${hitboxH}`)
+        Phaser.Display.Align.In.Center(this.player_two_action_hitbox_text, this.player_two_action_hitbox);
+    }   
+
+}
