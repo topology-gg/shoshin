31b89a6ed1d71aaca2a13c7ac5707a18a4a2f339
Matthew Demoy <0xNonCents@gmail.com>
2023-03-29 13:21:26 -0400

    ts-ignore dynamic import

--- tooling/pages/index.tsx
+++ tooling/pages/index.tsx
@@ -39,6 +39,7 @@ import { EditorTabName } from '../src/components/sidePanelComponents/Tabs';
 import { unwrapLeafToCondition, unwrapLeafToTree } from '../src/types/Leaf';
 import dynamic from "next/dynamic";
 
+//@ts-ignore
 const Game = dynamic(() => import("../src/Game/PhaserGame"), {
     ssr: false,
 });
