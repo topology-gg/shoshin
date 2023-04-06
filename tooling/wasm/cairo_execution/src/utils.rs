471c58bce833f200ce0b3845db0dc33a5a5e5602
Gregory Edison <gregory.edison1993@gmail.com>
2023-04-06 17:05:09 +0200

    update to execution utils

--- tooling/wasm/cairo_execution/src/utils.rs
+++ tooling/wasm/cairo_execution/src/utils.rs
@@ -1,16 +1,18 @@
-#[macro_export]
-macro_rules! bigint {
-    ($val : expr) => {
-        Into::<BigInt>::into($val)
-    };
-}
+use anyhow::Error;
+use cairo_vm::{types::relocatable::MaybeRelocatable, vm::runners::cairo_runner::CairoArg};
+use std::convert::Into;
 
-#[macro_export]
-macro_rules! mayberelocatable {
-    ($val1 : expr, $val2 : expr) => {
-        MaybeRelocatable::from(($val1, $val2))
-    };
-    ($val1 : expr) => {
-        MaybeRelocatable::from(Felt::new(bigint!($val1)))
-    };
+/// Convert Vec<i32> to Vec<CairoArg>
+/// # Arguments
+/// * `inputs` - The flattened inputs to the cairo program
+///
+/// # Returns
+/// The inputs converted to CairoArgs
+pub fn prepare_args<T: Into<Vec<CairoArg>>>(inputs: T) -> Result<Vec<CairoArg>, Error> {
+    let range_check_ptr = MaybeRelocatable::from((2, 0));
+
+    let mut args = vec![CairoArg::from(range_check_ptr)];
+    args.extend(Into::<Vec<CairoArg>>::into(inputs));
+
+    Ok(args)
 }
