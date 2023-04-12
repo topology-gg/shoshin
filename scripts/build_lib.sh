7ddbfb562deb9caa09429bd1851d40f6dcc73b13
Gregory Edison <gregory.edison1993@gmail.com>
2023-04-12 10:32:33 +0200

    update condition evaluation script

--- scripts/build_lib.sh
+++ scripts/build_lib.sh
@@ -5,6 +5,7 @@ echo "
 %builtins range_check
 from cairo_test.lib.tree import Tree, BinaryOperatorTree
 from starkware.cairo.common.dict_access import DictAccess
+from starkware.cairo.common.alloc import alloc
 from starkware.cairo.common.default_dict import default_dict_new
 from starkware.cairo.common.dict import dict_write
 
@@ -16,25 +17,24 @@ struct KeyValuePair {
 func evaluate_condition{range_check_ptr}(
     condition_len: felt,
     condition: Tree*,
-    mem_len: felt,
-    mem: felt*,
     dict_len: felt,
     dict: KeyValuePair*,
-) -> (output: felt, mem_len: felt, mem: felt*) {
+) -> (output: felt) {
     alloc_locals;
 
-    let (_unused) = default_dict_new(default_value=0);
+    let (_unused_dict) = default_dict_new(default_value=0);
+    let (_unused_mem) = alloc();
 
     let (loaded_dict) = default_dict_new(default_value=0);
     let (loaded_dict_new) = load_dict(dict_len, dict, loaded_dict);
-    let (output, _, _, mem_len_new) = BinaryOperatorTree.iterate_tree(
+    let (output, _, _, _) = BinaryOperatorTree.iterate_tree(
         condition,
-        mem_len,
-        mem,
-        _unused,
+        0,
+        _unused_mem,
+        _unused_dict,
         loaded_dict_new,
     );
-    return (output=output, mem_len=mem_len_new, mem=mem);
+    return (output=output);
 }
 
 func load_dict{range_check_ptr}(dict_len: felt, dict: KeyValuePair*, loaded_dict: DictAccess*) -> (loaded_dict_new: DictAccess*) {
