f3619aa74c267f5cba934e71ee2888b9b70084e0
0xNonCents <0xnoncents@gmail.com>
2023-05-26 14:24:55 -0400

    add use whitelist

--- tooling/lib/api.js
+++ tooling/lib/api.js
@@ -10,6 +10,10 @@ export function useLeagueAgents () {
     return useSWR('/api/agents/league', fetcher)
 }
 
+export function useWhitelist () {
+    return useSWR('/api/whitelist/whitelist', fetcher)
+}
+
 export function useStardiscRegistryByAccount (account) {
     return useSWR(`/api/stardisc_registry/${account}`, fetcher)
 }
