b900725485fde17ce2be20f24571bdbb9d87f9af
Cristian A <zink.krysty@gmail.com>
2023-07-21 12:30:30 +0200

    Click to play video

--- tooling/src/components/layout/SceneSelector.tsx
+++ tooling/src/components/layout/SceneSelector.tsx
@@ -84,7 +84,7 @@ const defaultOpponent: Opponent = {
 };
 const StorageKey = 'PersistedGameState';
 const SceneSelector = () => {
-    const [scene, setScene] = useState<Scene>(Scenes.MAIN_SCENE);
+    const [scene, setScene] = useState<Scene>(Scenes.WALLET_CONNECT);
 
     const ctx = React.useContext(ShoshinWASMContext);
 
