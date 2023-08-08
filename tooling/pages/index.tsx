6802d2c65182172a96377801dc38d15df29df986
Cristian A <zink.krysty@gmail.com>
2023-08-08 21:57:46 +0200

    Improve preloading of images and initial transitions
    Improves the experience on a poor connection, where background images took too much time to load

--- tooling/pages/index.tsx
+++ tooling/pages/index.tsx
@@ -2,17 +2,17 @@ import SceneSelector from '../src/components/layout/SceneSelector';
 import { StyledEngineProvider, ThemeProvider } from '@mui/material';
 import theme, { darkTheme } from '../src/theme/theme';
 import Head from 'next/head';
+import preloadImages from '../src/constants/preloadImages';
 
 export default function Home() {
     return (
         <StyledEngineProvider injectFirst>
             <ThemeProvider theme={darkTheme}>
                 <Head>
-                    <link
-                        rel="preload"
-                        href="/images/logo/shoshin-logo-big.png"
-                        as="image"
-                    />
+                    {preloadImages.map((img) => (
+                        <link rel="preload" href={img} as="image" />
+                    ))}
+
                     <link
                         rel="stylesheet"
                         href="https://fonts.googleapis.com/css?family=Dela+Gothic+One"
